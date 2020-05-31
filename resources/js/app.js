f1bd4c11b16c8a1f60580f6749e34611222ed103
smzfreemyt <samuel.amador@freemight.com>
2020-05-31 18:14:39 +0800

    Fresh lara7 copy

---
+++ resources/js/app.js
@@ -0,0 +1,20 @@
+import './bootstrap';
+import Vue from 'vue';
+import Routes from '@/js/routes.js';
+import App from '@/js/views/App';
+
+
+
+
+/**
+ * Next, we will create a fresh Vue application instance and attach it to
+ * the page. Then, you may begin adding components to this application
+ * or customize the JavaScript scaffolding to fit your unique needs.
+ */
+
+const app = new Vue({
+    el: '#app',
+    router: Routes,
+    render: h => h(App),
+});
+export default app;
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{csrf_token()}}">
+    <title>Laravel Airlock</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <link href="{{ mix('/css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+    @if (Auth::check())
+        <script>
+           window.Laravel = {!!json_encode([
+               'isLoggedin' => true,
+               'user' => Auth::user()
+           ])!!}
+        </script>
+    @else
+        <script>
+            window.Laravel = {!!json_encode([
+                'isLoggedin' => false
+            ])!!}
+        </script>
+    @endif
+    <div id="app"></div>
+    <script src="{{ mix('js/app.js') }}"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+</body>
+</html>
